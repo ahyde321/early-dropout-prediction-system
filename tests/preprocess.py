16d1f006d8dc2f05ffb2a2f846c63959ed0a77ad
ahyde321 <ahyde321@gmail.com>
2025-01-25 12:41:45 +0000

    raw enrolled data

--- tests/preprocess.py
+++ tests/preprocess.py
@@ -6,9 +6,11 @@ sys.path.append(base_dir)
 from pipeline.preprocess_student_dropout_data import preprocess_student_dropout_data
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-input_file = os.path.join(script_dir, '../data/raw/raw_dataset2.csv')
-train_file = os.path.join(script_dir, '../data/processed/preprocessed2.csv')
-test_file = os.path.join(script_dir, '../data/test/test_preprocessed2.csv')
+input_file = os.path.join(script_dir, '../data/raw/raw_dataset1.csv')
+train_file = os.path.join(script_dir, '../data/processed/preprocessed1.csv')
+test_file = os.path.join(script_dir, '../data/test/enrolled1.csv')
+raw_test = os.path.join(script_dir, '../data/test/raw_enrolled1.csv')
 
 
-preprocess_student_dropout_data(input_file, train_file, test_file)
+
+preprocess_student_dropout_data(input_file, train_file, test_file, raw_test)
