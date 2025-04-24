11ec22d266afa76d160a44bcc484f76e914b4a12
ahyde321 <ahyde321@gmail.com>
2025-04-24 18:52:47 +0100

    alembic and pgsql

--- db/database.py
+++ db/database.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./students.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")  # now uses your .env
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+import os
+print("✅ DATABASE_URL:", os.getenv("DATABASE_URL"))
